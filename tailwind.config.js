cfe5c77887536ab3f191f34a2899fc0da71635d1
Fernando Alba <fernando.alba@live.com.ar>
2022-04-11 22:26:42 -0300

    initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        neutral: '#f0f7fa',
+        primary: '#5493e7',
+        'primary-dark': '#132338',
+        accent: '#1cc589',
+      },
+    },
+  },
+  plugins: [],
+}
