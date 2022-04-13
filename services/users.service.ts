a2542b5e25abcba4de5107f3c3681da79a6a41c7
Fernando Alba <fernando.alba@live.com.ar>
2022-04-13 10:25:13 -0300

    refactor user services using axios-call model

--- services/users.service.ts
+++ services/users.service.ts
@@ -1,22 +1,34 @@
+import { AxiosCall } from '@/models/axios-call.model'
 import { User } from '@/models/user.model'
-import axios, { AxiosPromise } from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 export const api = axios.create({
   baseURL: 'https://wired-torus-98413.firebaseio.com',
 })
 
-export const getUsers = () => {
-  return api
-    .get('/users.json')
+export const getUsers = (): AxiosCall<any> => {
+  const controller = new AbortController()
+
+  const call: Promise<AxiosResponse<any>> = api
+    .get('/users.json', {
+      signal: controller.signal,
+    })
     .then((response) => {
       return response.data
     })
     .catch((error) => {
       console.error(error.message)
     })
+
+  return {
+    call: call,
+    controller,
+  }
 }
 
-export const postUser = ({ user }: { user: User }) => {
+export const postUser = ({ user }: { user: User }): AxiosCall<any> => {
+  const controller = new AbortController()
+
   const data = JSON.stringify({
     dni: user.dni,
     email: user.email,
@@ -26,18 +38,39 @@ export const postUser = ({ user }: { user: User }) => {
     name: user.name,
   })
 
-  return api
-    .post('/users.json', data)
+  const call: Promise<AxiosResponse<any>> = api
+    .post('/users.json', data, {
+      signal: controller.signal,
+    })
     .then((response) => {
       return response.data
     })
     .catch((error) => {
       console.error(error.message)
     })
+
+  return {
+    call: call,
+    controller,
+  }
 }
 
-export const deleteUser = ({ id }: { id: string }) => {
-  return api.delete(`/users/${id}.json`).catch((error) => {
-    console.error(error.message)
-  })
+export const deleteUser = ({ id }: { id: string }): AxiosCall<any> => {
+  const controller = new AbortController()
+
+  const call: Promise<void | AxiosResponse<any>> = api
+    .delete(`/users/${id}.json`, {
+      signal: controller.signal,
+    })
+    .then((response) => {
+      return response
+    })
+    .catch((error) => {
+      console.error(error.message)
+    })
+
+  return {
+    call: call as Promise<AxiosResponse<any>>,
+    controller: controller,
+  }
 }
