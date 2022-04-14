ec5ea6388048683a8b0bc492b3f471f6dc8129bd
Fernando Alba <fernando.alba@live.com.ar>
2022-04-14 15:03:54 -0300

    create Navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+function Navbar(): JSX.Element {
+  return (
+    <header className="bg-neutral shadow-md">
+      <nav className="container mx-auto p-5">
+        <Link href="/">
+          <a>
+            <div className="w-[80px] lg:w-[123px]">
+              <Image
+                src="/moni-logo-primary.svg"
+                alt="Moni Logo"
+                width={80}
+                height={30}
+                layout="responsive"
+              />
+            </div>
+          </a>
+        </Link>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
