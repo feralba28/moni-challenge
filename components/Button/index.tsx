9dce7874614292b3264432292800b1c59764376a
Fernando Alba <fernando.alba@live.com.ar>
2022-04-14 15:06:35 -0300

    create Button component

---
+++ components/Button/index.tsx
@@ -0,0 +1,29 @@
+import { type } from 'os'
+import { ReactNode } from 'react'
+
+import styles from './styles.module.css'
+
+interface Props {
+  variant: 'primary' | 'secondary' | 'accent'
+  children: ReactNode
+  type?: 'button' | 'submit' | 'reset'
+  disabled?: boolean
+}
+
+function Button(props: Props): JSX.Element {
+  const { variant, children, type, disabled } = props
+
+  return (
+    <button
+      className={`${styles.btn} ${styles[variant]} ${
+        disabled ? styles.disabled : ''
+      }`}
+      type={type}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
