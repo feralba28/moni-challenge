3d741bb38b534fa72959fb164b1182bfc642f59b
Fernando Alba <fernando.alba@live.com.ar>
2022-04-14 15:05:02 -0300

    create PageLayout component

---
+++ components/PageLayout.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react'
+
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
+
+function PageLayout(props: { children: ReactNode }): JSX.Element {
+  const { children } = props
+
+  return (
+    <div className="min-h-full bg-neutral flex flex-col">
+      <Navbar />
+
+      {children}
+
+      <Footer />
+    </div>
+  )
+}
+
+export default PageLayout
