fee66d3a3bfee828133687b2d1bc748cd2e004ec
Fernando Alba <fernando.alba@live.com.ar>
2022-04-14 15:02:25 -0300

    refactor _app.tsx and _document.tsx files

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta
+          name="description"
+          content="Préstamos en efectivo. Pedime hasta $130.000 pesos y te deposito en el momento. Fácil y rápido."
+        />
+        <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
