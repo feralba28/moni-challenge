fcc63faa2bcf3263b821ca45a7bfb0749a1374a9
Fernando Alba <fernando.alba@live.com.ar>
2022-04-16 10:50:31 -0300

    create userAdapter and refactor postUserAdapter

--- adapters/user.adapter.ts
+++ adapters/user.adapter.ts
@@ -1,17 +1,16 @@
 import { FormValues } from '@/models/form-values.model'
-import { Score } from '@/models/score.model'
 import { User, Users } from '@/models/user.model'
 
 export const createPostUserAdapter = (
   values: FormValues,
-  score: Score
+  status: string
 ): User => {
   const user: User = {
     dni: values.dni.toString(),
     email: values.email,
     genre: values.gender,
     last: values.lastName,
-    loanStatus: score.status,
+    loanStatus: status,
     name: values.firstName,
   }
 
@@ -22,4 +21,15 @@ export const createUsersAdapter = (users: any): Users => {
   return users
     ? new Map<string, User>(Object.entries(users))
     : new Map<string, User>()
+}
+
+export const createUserAdapter = (user: any): User => {
+  return {
+    dni: user.dni,
+    email: user.email,
+    genre: user.genre,
+    last: user.last,
+    loanStatus: user.loanStatus,
+    name: user.name,
+  }
 }
