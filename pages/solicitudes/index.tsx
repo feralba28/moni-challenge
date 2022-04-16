3f072e12b5803928996ec478edee3353bde806d2
Fernando Alba <fernando.alba@live.com.ar>
2022-04-16 10:57:27 -0300

    feat: update user on requests listing page

--- pages/solicitudes/index.tsx
+++ pages/solicitudes/index.tsx
@@ -3,15 +3,27 @@ import Head from 'next/head'
 
 import { useState } from 'react'
 import { AxiosResponse } from 'axios'
+import { Formik, Form, FormikHelpers } from 'formik'
+import ClipLoader from 'react-spinners/ClipLoader'
 
 import { formatStatus } from '@/adapters/score.adapter'
-import { createUsersAdapter } from '@/adapters/user.adapter'
-import { Body, Cell, Header, Item, Row, Table } from '@/components/Table'
+import {
+  createPostUserAdapter,
+  createUserAdapter,
+  createUsersAdapter,
+} from '@/adapters/user.adapter'
+import Button from '@/components/Button'
+import CloseIcon from '@/components/CloseIcon'
+import { SelectField, TextField } from '@/components/Fields'
+import Modal from '@/components/Modal'
 import PageLayout from '@/components/PageLayout'
-import useFetch from '@/hooks/useFetch'
-import { Users } from '@/models/user.model'
-import { deleteUser, getUsers } from '@/services/users.service'
+import { Body, Cell, Header, Item, Row, Table } from '@/components/Table'
 import TextButton from '@/components/TextButton'
+import useFetch from '@/hooks/useFetch'
+import { FormValues } from '@/models/form-values.model'
+import { User, Users } from '@/models/user.model'
+import { deleteUser, getUsers, updateUser } from '@/services/users.service'
+import { userSchema } from '@/validations/user.validation'
 
 const tableColums: Array<string> = [
   'Nombre',
@@ -25,10 +37,16 @@ const tableColums: Array<string> = [
 
 const Prestamos: NextPage<{ users: any }> = ({ users: usersProp }) => {
   const [users, setUsers] = useState<Users>(() => createUsersAdapter(usersProp))
+  const [isModal, setIsModal] = useState<boolean>(false)
+  const [selectedUser, setSelectedUser] = useState<{
+    id: string
+    user: User
+  } | null>(null)
 
   const { loading, callEndpoint: callDeleteUser } = useFetch()
+  const { callEndpoint: callUpdateUser } = useFetch()
 
-  const handleDelete = async (id: string) => {
+  const handleOnDelete = async (id: string) => {
     try {
       const response = await callDeleteUser(deleteUser({ id }))
       if (response.status === 200) {
@@ -42,7 +60,34 @@ const Prestamos: NextPage<{ users: any }> = ({ users: usersProp }) => {
     }
   }
 
-  const handleUpdate = (id: string) => {}
+  const handleOnUpdate = (id: string) => {
+    setSelectedUser({ id: id, user: users.get(id)! })
+    setIsModal(true)
+  }
+
+  const onSubmitUpdate = async (
+    values: FormValues,
+    { setSubmitting }: FormikHelpers<FormValues>
+  ) => {
+    const {
+      id,
+      user: { loanStatus },
+    } = selectedUser!
+    const user = createPostUserAdapter(values, loanStatus)
+    try {
+      const response = await callUpdateUser(updateUser({ id: id, user: user }))
+      if (response.status == 200) {
+        const draft = users
+        draft.set(id, createUserAdapter(response.data))
+        setUsers(draft)
+        console.log('Los datos se actualizaron correctamente', response.data)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+    setSubmitting(false)
+    setIsModal(false)
+  }
 
   return (
     <>
@@ -52,59 +97,127 @@ const Prestamos: NextPage<{ users: any }> = ({ users: usersProp }) => {
 
       <PageLayout>
         <main className="container mx-auto px-5 py-8 grow">
-          {loading ? (
-            <div>spinner</div>
-          ) : (
+          {loading && (
+            <div className="bg-white/80 fixed inset-0 z-50 h-full flex justify-center items-center">
+              <ClipLoader loading={true} size={48} color="#5493e7" />
+            </div>
+          )}
+
+          {[...users.keys()].length > 0 ? (
             <Table title="Solicitudes">
               <Header colums={tableColums} />
               <Body>
-                {[...users.keys()].map((id: string) => (
-                  <Row key={id}>
-                    <Cell>
-                      <Item label={'Nombre'} value={users.get(id)!.name} />
-                    </Cell>
-                    <Cell>
-                      <Item label={'Apellido'} value={users.get(id)!.last} />
-                    </Cell>
-                    <Cell>
-                      <Item label={'DNI'} value={users.get(id)!.dni} />
-                    </Cell>
-                    <Cell>
-                      <Item label={'Género'} value={users.get(id)!.genre} />
-                    </Cell>
-                    <Cell>
-                      <Item label={'Email'} value={users.get(id)!.email} />
-                    </Cell>
-                    <Cell>
-                      <Item
-                        label={'Estado'}
-                        value={formatStatus(users.get(id)!.loanStatus)}
-                      />
-                    </Cell>
-                    <Cell>
-                      <div className="flex items-center">
-                        <span className="inline-block w-1/3 lg:hidden font-bold text-sm text-gray-900">
-                          Acciones
-                        </span>
-                        <div className="w-2/3 flex gap-4">
-                          <TextButton
-                            text="Editar"
-                            variant="accent"
-                            onClick={() => handleUpdate}
-                          />
-                          <TextButton
-                            text="Eliminar"
-                            variant="warning"
-                            onClick={() => handleDelete(id)}
-                          />
+                {[...users.keys()].map((id: string) => {
+                  const { name, last, dni, genre, email, loanStatus } =
+                    users.get(id)!
+                  return (
+                    <Row key={id}>
+                      <Cell>
+                        <Item label="Nombre" value={name} />
+                      </Cell>
+                      <Cell>
+                        <Item label="Apellido" value={last} />
+                      </Cell>
+                      <Cell>
+                        <Item label="DNI" value={dni} />
+                      </Cell>
+                      <Cell>
+                        <Item label="Género" value={genre} />
+                      </Cell>
+                      <Cell>
+                        <Item label="Email" value={email} />
+                      </Cell>
+                      <Cell>
+                        <Item label="Estado" value={formatStatus(loanStatus)} />
+                      </Cell>
+                      <Cell>
+                        <div className="flex items-center">
+                          <span className="inline-block w-1/3 lg:hidden font-bold text-sm text-gray-900">
+                            Acciones
+                          </span>
+                          <div className="w-2/3 flex gap-4">
+                            <TextButton
+                              text="Editar"
+                              variant="accent"
+                              onClick={() => handleOnUpdate(id)}
+                            />
+                            <TextButton
+                              text="Eliminar"
+                              variant="warning"
+                              onClick={() => handleOnDelete(id)}
+                            />
+                          </div>
                         </div>
-                      </div>
-                    </Cell>
-                  </Row>
-                ))}
+                      </Cell>
+                    </Row>
+                  )
+                })}
               </Body>
             </Table>
+          ) : (
+            <h1 className="text-center text-primary-dark text-xl font-bold mb-4 lg:text-2xl">
+              No hay solicitudes
+            </h1>
           )}
+
+          <Modal isModal={isModal}>
+            <Formik
+              initialValues={{
+                firstName: selectedUser?.user.name as string,
+                lastName: selectedUser?.user.last as string,
+                gender: selectedUser?.user.genre as string,
+                email: selectedUser?.user.email as string,
+                dni: selectedUser?.user.dni as string,
+              }}
+              validationSchema={userSchema}
+              onSubmit={onSubmitUpdate}
+            >
+              {({ isSubmitting, errors }) => (
+                <Form className="w-full bg-white rounded-lg p-8 flex flex-col gap-2 shadow-md lg:p-10">
+                  <div className="flex items-center justify-between">
+                    <p className="text-center text-primary-dark text-xl font-bold py-2 lg:text-2xl">
+                      Editar solicitud
+                    </p>
+                    <button
+                      className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-md text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white"
+                      onClick={() => setIsModal(false)}
+                    >
+                      <CloseIcon />
+                    </button>
+                  </div>
+
+                  <TextField label="Nombre" name="firstName" type="text" />
+
+                  <TextField label="Apellido" name="lastName" type="text" />
+
+                  <SelectField label="Género" name="gender">
+                    <option value="">Seleccione un género</option>
+                    <option value="Masculino">Masculino</option>
+                    <option value="Femenino">Femenino</option>
+                    <option value="No Binario">No Binario</option>
+                  </SelectField>
+
+                  <TextField label="Email" name="email" type="text" />
+
+                  <TextField label="DNI" name="dni" type="number" disabled />
+
+                  <Button
+                    variant="accent"
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Actualizar
+                  </Button>
+
+                  {isSubmitting && (
+                    <div className="flex justify-center mt-1 lg:mt-2">
+                      <ClipLoader loading={true} size={36} color="#5493e7" />
+                    </div>
+                  )}
+                </Form>
+              )}
+            </Formik>
+          </Modal>
         </main>
       </PageLayout>
     </>
