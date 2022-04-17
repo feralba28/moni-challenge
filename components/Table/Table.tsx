2c10a883b4bf623017bb486ce50df4faa5b6e17b
Fernando Alba <fernando.alba@live.com.ar>
2022-04-17 11:45:26 -0300

    add shadow to table component

--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 
 const Table = ({ children, title }: { children: ReactNode; title: string }) => {
   return (
-    <div className="max-w-md mx-auto bg-white rounded-lg p-4 lg:max-w-6xl lg:p-8">
+    <div className="max-w-md mx-auto bg-white rounded-lg p-4 shadow-md lg:max-w-6xl lg:p-8">
       <h1 className="text-center text-primary-dark text-xl font-bold mb-4 lg:text-2xl">
         {title}
       </h1>
